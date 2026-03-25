5a1f0e76f0d66067a47c4789cf40d54cb53ce1f8
MirajMustakim <mirajmustakim000@gmail.com>
2026-03-25 13:50:28 -0400

    Updated Grade Checker

--- basics/conditionals/grade_checker.py
+++ basics/conditionals/grade_checker.py
@@ -1,10 +1,16 @@
-marks = int(input("Enter your marks: "))
+marks = int(input("Enter marks: "))
 
-if marks >= 90:
-    print("Grade A")
-elif marks >= 75:
-    print("Grade B")
-elif marks >= 50:
-    print("Grade C")
+if marks < 0 or marks > 100:
+    print("Invalid marks")
+
+elif marks < 50:
+    print("Failed")
+
+elif marks >= 80:
+    print("Passed - Grade A")
+
+elif marks >= 60:
+    print("Passed - Grade B")
+
 else:
-    print("Fail")
+    print("Passed - Grade C")
